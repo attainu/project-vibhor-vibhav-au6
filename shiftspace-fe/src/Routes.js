b0346f6e103c003ecd8f5d9c7ce3f0a28cdc843e
vibhor <vibhorsmiki@gmail.com>
2020-09-30 05:54:08 +0530

    filter by price and category

--- shiftspace-fe/src/Routes.js
+++ shiftspace-fe/src/Routes.js
@@ -3,26 +3,27 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./components/user/Signup";
 import Signin from "./components/user/Signin";
 import Home from "./components/core/Home";
-import Shop from "./components/core/Shop";
-
-import PrivateRoute from "./components/auth/PrivateRoute"
-import Dashboard from "./components/user/UserDashboard"
-import AdminRoute from "./components/auth/AdminRoute"
-import AdminDashboard from "./components/user/AdminDashboard"
+import PrivateRoute from "./components/auth/PrivateRoute";
+import Dashboard from "./components/user/UserDashboard";
+import AdminRoute from "./components/auth/AdminRoute";
+import AdminDashboard from "./components/user/AdminDashboard";
 import AddCategory from "./components/admin/AddCategory";
 import AddProduct from "./components/admin/AddProduct";
+import Shop from "./components/core/Shop";
 
-const App = () => {
+const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/shop" exact component={Shop} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
                 <PrivateRoute
-                path="/user/dashboard"
-                exact
-                component={Dashboard} />
+                    path="/user/dashboard"
+                    exact
+                    component={Dashboard}
+                />
                 <AdminRoute
                     path="/admin/dashboard"
                     exact
@@ -43,4 +44,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default Routes;
--- shiftspace-fe/src/components/core/Menu.js
+++ shiftspace-fe/src/components/core/Menu.js
@@ -22,6 +22,15 @@ const Menu = ({ history }) => (
                     Home
                 </Link>
             </li>
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/shop")}
+                    to="/shop"
+                >
+                    Search & Filter
+                </Link>
+            </li>
 
             {isAuthenticated() && isAuthenticated().user.role === 0 && (
                 <li className="nav-item">
